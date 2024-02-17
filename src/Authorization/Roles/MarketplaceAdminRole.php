e4184d571972589c43991688c5d124c44fdbe238
Harun Baris Bulut <baris.bulut@plusclouds.com>
2024-02-17 23:51:34 +0300

    Authorization enhanced

---
+++ src/Authorization/Roles/MarketplaceAdminRole.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace NextDeveloper\Marketplace\Authorization\Roles;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use NextDeveloper\CRM\Database\Models\AccountManagers;
+use NextDeveloper\IAM\Authorization\Roles\AbstractRole;
+use NextDeveloper\IAM\Authorization\Roles\IAuthorizationRole;
+use NextDeveloper\IAM\Database\Models\Users;
+use NextDeveloper\IAM\Helpers\UserHelper;
+
+class MarketplaceAdminRole extends AbstractRole implements IAuthorizationRole
+{
+    public const NAME = 'marketplace-admin';
+
+    public const LEVEL = 50;
+
+    public const DESCRIPTION = 'Marketplace Admin';
+
+    public const DB_PREFIX = 'marketplace';
+
+    /**
+     * Applies basic member role sql for Eloquent
+     *
+     * @param Builder $builder
+     * @param Model $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        /**
+         * Here user will be able to list all models, because by default, sales manager can see everybody.
+         */
+        $ids = AccountManagers::withoutGlobalScopes()
+            ->where('iam_account_id', UserHelper::currentAccount()->id)
+            ->pluck('crm_account_id');
+
+        $builder->whereIn('iam_account_id', $ids);
+    }
+
+    public function checkPrivileges(Users $users = null)
+    {
+        //return UserHelper::hasRole(self::NAME, $users);
+    }
+
+    public function getModule()
+    {
+        return 'marketplace';
+    }
+
+    public function allowedOperations() :array
+    {
+        return [
+            'marketplace_products:read',
+            'marketplace_products:update',
+            'marketplace_products:create',
+            'marketplace_products:delete',
+            'marketplace_product_catalogs:read',
+            'marketplace_product_catalogs:update',
+            'marketplace_product_catalogs:create',
+            'marketplace_product_catalogs:delete',
+            'marketplace_subscriptions:read',
+            'marketplace_subscriptions:update',
+            'marketplace_subscriptions:create',
+            'marketplace_subscriptions:delete'
+        ];
+    }
+
+    public function getLevel(): int
+    {
+        return self::LEVEL;
+    }
+
+    public function getDescription(): string
+    {
+        return self::DESCRIPTION;
+    }
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    public function canBeApplied($column)
+    {
+        if(self::DB_PREFIX === '*') {
+            return true;
+        }
+
+        if(Str::startsWith($column, self::DB_PREFIX)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getDbPrefix()
+    {
+        return self::DB_PREFIX;
+    }
+}
