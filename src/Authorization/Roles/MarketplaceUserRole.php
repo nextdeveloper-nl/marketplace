207090e90e89b5eb73cffe626f64b81501d031df
Harun Baris Bulut <baris.bulut@plusclouds.com>
2024-03-16 17:17:24 +0300

    New role added

---
+++ src/Authorization/Roles/MarketplaceUserRole.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace NextDeveloper\Marketplace\Authorization\Roles;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use NextDeveloper\CRM\Database\Models\AccountManagers;
+use NextDeveloper\IAM\Authorization\Roles\AbstractRole;
+use NextDeveloper\IAM\Authorization\Roles\IAuthorizationRole;
+use NextDeveloper\IAM\Database\Models\Users;
+use NextDeveloper\IAM\Helpers\UserHelper;
+
+class MarketplaceUserRole extends AbstractRole implements IAuthorizationRole
+{
+    public const NAME = 'marketplace-user';
+
+    public const LEVEL = 30;
+
+    public const DESCRIPTION = 'Marketplace user can see and subscribe to marketplace items.';
+
+    public const DB_PREFIX = 'marketplace';
+
+    /**
+     * Applies basic member role sql for Eloquent
+     *
+     * @param Builder $builder
+     * @param Model $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        /**
+         * Here user will be able to list all models, because by default, sales manager can see everybody.
+         */
+
+    }
+
+    public function checkPrivileges(Users $users = null)
+    {
+        //return UserHelper::hasRole(self::NAME, $users);
+    }
+
+    public function getModule()
+    {
+        return 'marketplace';
+    }
+
+    public function allowedOperations() :array
+    {
+        return [
+            'marketplace_markets:read',
+            'marketplace_products:read',
+            'marketplace_product_catalogs:read',
+            'marketplace_subscriptions:read',
+            'marketplace_subscriptions:update',
+            'marketplace_subscriptions:create'
+        ];
+    }
+
+    public function getLevel(): int
+    {
+        return self::LEVEL;
+    }
+
+    public function getDescription(): string
+    {
+        return self::DESCRIPTION;
+    }
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    public function canBeApplied($column)
+    {
+        if(self::DB_PREFIX === '*') {
+            return true;
+        }
+
+        if(Str::startsWith($column, self::DB_PREFIX)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getDbPrefix()
+    {
+        return self::DB_PREFIX;
+    }
+}
