f2017adee61d2a1645d8c729344f1ed3bf5baab7
Harun Baris Bulut <baris.bulut@plusclouds.com>
2024-12-08 00:26:48 +0300

    Added public products perspective

--- src/Services/ProductsPerspectiveService.php
+++ src/Services/ProductsPerspectiveService.php
@@ -5,6 +5,8 @@ namespace NextDeveloper\Marketplace\Services;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Str;
+use NextDeveloper\Commons\Database\GlobalScopes\LimitScope;
+use NextDeveloper\IAM\Database\Scopes\AuthorizationScope;
 use NextDeveloper\Marketplace\Database\Filters\ProductsQueryFilter;
 use NextDeveloper\Marketplace\Database\Models\ProductCatalogs;
 use NextDeveloper\Marketplace\Database\Models\ProductsPerspective;
@@ -21,6 +23,19 @@ class ProductsPerspectiveService extends AbstractProductsPerspectiveService
 {
 
     // EDIT AFTER HERE - WARNING: ABOVE THIS LINE MAY BE REGENERATED AND YOU MAY LOSE CODE
+    public static function getPublicProducts() : ?Collection
+    {
+        $products = ProductsPerspective::withoutGlobalScope(AuthorizationScope::class)
+            ->withoutGlobalScope(LimitScope::class)
+            ->where('is_public', true)
+            ->where('is_active', true)
+            ->where('is_in_maintenance', false)
+            ->where('is_invisible', false)
+            ->get();
+
+        return $products;
+    }
+
     public static function getBySlug($slug) : ?ProductsPerspective
     {
         $product = ProductsPerspective::where('slug', $slug)->first();
